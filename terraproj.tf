17871cc8721b4a3638db547c822b6329bb8461f3
lmk <lordmayorkevin@gmail.com>
2023-03-02 08:52:28 -0600

    EC2 PROJECT
    i'm making a project on ec2

--- terraproj.tf
+++ terraproj.tf
@@ -14,3 +14,29 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "mainvpc"
+  }
+}
+
+resource "aws_subnet" "mainpub" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "publicsub"
+  }
+}
+
+resource "aws_subnet" "mainpriv" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "privatesub"
+  }
+}
